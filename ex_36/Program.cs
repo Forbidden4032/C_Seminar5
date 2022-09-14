0ffb0609e301a4bff96815a03d4fa68df665dfbd
forbidden <forbidden@fedora.com>
2022-09-14 23:13:52 +0300

    add ex_36

---
+++ ex_36/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+int[] CreateArray(int size)
+    {
+        int[] array = new int[size];
+        var random = new Random();
+        for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+            }
+    return array;
+    }
+
+int Summ(int[] array)
+    {
+        int summ = 0;
+        for(int i = 0; i < array.Length; i++)
+            {
+                if(i%2 != 0)
+                    {
+                        summ = summ + array[i];
+                    }
+            }
+    return summ;
+    }
+
+int[] newArray = CreateArray(10);
+int newSumm = Summ(newArray);
+Console.WriteLine($"Сумма элементов на нечетных позициях: {newSumm}");
+
+
+
