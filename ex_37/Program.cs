84e65788fcdbfc84e32afadf7fd25fd06cad2c35
forbidden <forbidden@fedora.com>
2022-09-14 23:50:34 +0300

    add ex_37

---
+++ ex_37/Program.cs
@@ -0,0 +1,22 @@
+﻿//Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+
+int[] Array(int[] array)
+{
+    int[] NewArray = new int [array.Length/2];
+    for(int i = 0, j = array.Length - 1; i < (array.Length/2); i++, j-- )
+        {
+            NewArray[i] = array[i] * array[j];
+        }
+    return NewArray;
+}
+
+int[] array = new int [] {1,4,7,8,9,5,6};
+int[] ShowArray = Array(array);
+for(int i = 0; i < ShowArray.Length; i++)
+    {
+        Console.WriteLine(ShowArray[i]);
+    }
+
+
